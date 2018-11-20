0d4e5f0fc2806f711211eda0926fde1f20e31578
Isaac <ialb998>
2018-11-20 00:36:13 -0500

    adding initial files

---
+++ cgml_final_project/cifar100tree.py
@@ -0,0 +1,181 @@
+from __future__ import print_function
+import keras
+from keras.datasets import cifar100
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras import optimizers
+import numpy as np
+from keras.layers.core import Lambda
+from keras import backend as K
+from keras import regularizers
+
+class cifar100tree:
+	def __init__(self,weights=None):
+		self.num_classes = 100
+		self.weight_decay = 0.0005
+		self.x_shape = [32,32,3]
+
+		self.model = self.build_model()
+		if (weights):
+			self.model.load_weights(weights)
+		# self.model = self.train(self.model)
+		
+	def build_model(self):
+		inp = Input(shape=self.x_shape)
+
+		weight_decay = self.weight_decay
+
+		conv1_c = Conv2D(64, (3, 3), padding='same',
+						 input_shape=self.x_shape,kernel_regularizer=regularizers.l2(weight_decay))
+		conv1_a = Activation('relu')
+		conv1_b = BatchNormalization()
+		conv1_d = Dropout(0.3)
+
+		conv1 = conv1_d(conv1_b(conv1_a(conv1_c(inp))))
+
+		conv2_c = Conv2D(64, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv2_a = Activation('relu')
+		conv2_b = BatchNormalization()
+		conv2_p = MaxPooling2D(pool_size=(2, 2))
+
+		conv2 = conv2_p(conv2_b(conv2_a(conv2_c(conv1))))
+
+		conv3_c = Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv3_a = Activation('relu')
+		conv3_b = BatchNormalization()
+		conv3_d = Dropout(0.4)
+
+		conv3 = conv3_d(conv3_b(conv3_a(conv3_c(conv2))))
+
+		conv4_c = Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv4_a = Activation('relu')
+		conv4_b = BatchNormalization()
+		conv4_p = MaxPooling2D(pool_size=(2, 2))
+
+		conv4 = conv4_p(conv4_b(conv4_a(conv4_c(conv3))))
+
+		conv5_c = Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv5_a = Activation('relu')
+		conv5_b = BatchNormalization()
+		conv5_d = Dropout(0.4)
+
+		conv5 = conv5_d(conv5_b(conv5_a(conv5_c(conv4))))
+
+		conv6_c = Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv6_a = Activation('relu')
+		conv6_b = BatchNormalization()
+		conv6_d = Dropout(0.4)
+
+		conv6 = conv6_d(conv6_b(conv6_a(conv6_c(conv5))))
+
+		conv7_c = Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv7_a = Activation('relu')
+		conv7_b = BatchNormalization()
+		conv7_p = MaxPooling2D(pool_size=(2, 2))
+
+		conv7 = conv7_p(conv7_b(conv7_a(conv7_c(conv6))))
+
+		conv8_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv8_a = Activation('relu')
+		conv8_b = BatchNormalization()
+		conv8_d = Dropout(0.4)
+
+		conv8 = conv8_d(conv8_b(conv8_a(conv8_c(conv7))))
+
+		conv9_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv9_a = Activation('relu')
+		conv9_b = BatchNormalization()
+		conv9_d = Dropout(0.4)
+
+		conv9 = conv9_d(conv9_b(conv9_a(conv9_c(conv8))))
+
+		conv10_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv10_a = Activation('relu')
+		conv10_b = BatchNormalization()
+		conv10_p = MaxPooling2D(pool_size=(2, 2))
+
+		conv10 = conv10_p(conv10_b(conv10_a(conv10_c(conv9))))
+
+		conv11_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv11_a = Activation('relu')
+		conv11_b = BatchNormalization()
+		conv11_d = Dropout(0.4)
+
+		conv11 = conv11_d(conv11_b(conv11_a(conv11_c(conv10))))
+
+		conv12_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv12_a = Activation('relu')
+		conv12_b = BatchNormalization()
+		conv12_d = Dropout(0.4)
+
+		conv12 = conv12_d(conv12_b(conv12_a(conv12_c(conv11))))
+
+		conv13_c = Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))
+		conv13_a = Activation('relu')
+		conv13_b = BatchNormalization()
+		conv13_p = MaxPooling2D(pool_size=(2, 2))
+		conv13_d = Dropout(0.5)
+		conv13_f = Flatten()
+
+		conv13 = conv13_f(conv13_d(conv13_p(conv13_b(conv13_a(conv13_c(conv12))))))
+		
+		dense1_d = Dense(512,kernel_regularizer=regularizers.l2(weight_decay))
+		dense1_a = Activation('relu')
+		dense1_b = BatchNormalization()
+		dense1_do = Dropout(0.5)
+
+		dense1 = dense1_do(dense1_b(dense1_a(dense1_d(conv13))))
+
+		dense2_d = Dense(self.num_classes)
+		dense2_a = Activation('softmax')
+
+		dense2 = dense2_a(dense2_d(dense1))
+
+		model = Model(inputs=inp,outputs=dense2)
+		
+		return model
+
+	def normalize(self,X_train,X_test):
+		#this function normalize inputs for zero mean and unit variance
+		# it is used when training a model.
+		# Input: training set and test set
+		# Output: normalized training set and test set according to the trianing set statistics.
+		mean = np.mean(X_train,axis=(0,1,2,3))
+		std = np.std(X_train, axis=(0, 1, 2, 3))
+		print(mean)
+		print(std)
+		X_train = (X_train-mean)/(std+1e-7)
+		X_test = (X_test-mean)/(std+1e-7)
+		return X_train, X_test
+
+	def normalize_production(self,x):
+		#this function is used to normalize instances in production according to saved training set statistics
+		# Input: X - a training set
+		# Output X - a normalized training set according to normalization constants.
+
+		#these values produced during first training and are general for the standard cifar10 training set normalization
+		mean = 121.936
+		std = 68.389
+		return (x-mean)/(std+1e-7)
+
+	def predict(self,x,normalize=True,batch_size=50):
+		if normalize:
+			x = self.normalize_production(x)
+		return self.model.predict(x,batch_size)
+
+if __name__ == '__main__':
+	(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+	x_train = x_train.astype('float32')
+	x_test = x_test.astype('float32')
+
+	y_train = keras.utils.to_categorical(y_train, 100)
+	y_test = keras.utils.to_categorical(y_test, 100)
+
+	model = cifar100tree(weights="weights/cifar100vgg.h5")
+
+	predicted_x = model.predict(x_test)
+	residuals = (np.argmax(predicted_x,1)!=np.argmax(y_test,1))
+	loss = sum(residuals)/len(residuals)
+	print("the validation 0/1 loss is: ",loss)
