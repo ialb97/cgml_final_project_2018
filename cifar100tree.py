0bec0bf8f2cb4128a4b1f098dd338de7ddd60733
gavin <horseantson@hotmail.com>
2018-11-26 00:54:51 -0500

    lol

--- cifar100tree.py
+++ cifar100tree.py
@@ -263,14 +263,15 @@ class cifar100tree:
 				batches_per[rng] -= 1
 				x_batch,y_batch = batches[rng][batches_per[rng]]
 				self.model_dict[keys[rng]].train_on_batch(x_batch,y_batch)
-				print("Batch:{:3.0f}/{}".format(i,num_batches),end='\r')
+				print("Batch:{}/{}".format(i,num_batches),end='\r')
 			else:
 				i -= 1
 
 		# pdb.set_trace()
 		for model in self.model_dict:
 			self.model_dict[model].save_weights('weights/cifar100tree_{}.h5'.format(model))
-		print("accuracy: {}".format(self.eval_on_batch(datagen.flow(self.val_x_batches,self.val_y_batches,batch_size=1))))
+		x_batch,y_batch = datagen.flow(self.val_x_batches,self.val_y_batches,batch_size=1)
+		print("accuracy: {}".format(self.eval_on_batch(x_batch,y_batch)))
 
 	def get_root_mapping(self):
 		self.root_mapping = [0]*len(self.tree)
