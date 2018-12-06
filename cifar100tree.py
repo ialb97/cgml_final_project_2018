90cd44327c3c031d59eeb7f3134230ae688e2fac
gylee1618 <horseantson@hotmail.com>
2018-12-06 00:04:28 -0500

    merge and push fixed dir file

--- cifar100tree.py
+++ cifar100tree.py
@@ -401,7 +401,7 @@ if __name__ == '__main__':
 	xc_train = xc_train/255
 	xc_test = xc_test/255
 
-	model = cifar100tree(weights='weights/cifar100vgg.h5',load_weights=False,save_acc='accuracy/cifar100tree.csv',train=True)
+	model = cifar100tree(weights='weights/cifar100vgg.h5',load_weights=False,save_acc='metrics/cifar100tree.csv',train=True)
 
 	test_acc = model.predict(x_test,y_test)
 	val_acc = model.predict(x_train[::10],y_train[::10])
