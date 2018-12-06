d08c545e60161d40cd3779b93ff674c3a7846d66
gylee1618 <horseantson@hotmail.com>
2018-12-06 10:36:12 -0500

    merge, no more train just test on bad tree

--- cifar100badtree.py
+++ cifar100badtree.py
@@ -401,7 +401,7 @@ if __name__ == '__main__':
 	xc_train = xc_train/255
 	xc_test = xc_test/255
 
-	model = cifar100tree(weights='weights/cifar100vgg.h5',load_weights=False,save_acc=None,train=True)
+	model = cifar100tree(weights='weights/cifar100vgg.h5',load_weights=True,save_acc=None,train=False)
 
 	test_acc = model.predict(x_test,y_test)
 	val_acc = model.predict(x_train[::10],y_train[::10])
